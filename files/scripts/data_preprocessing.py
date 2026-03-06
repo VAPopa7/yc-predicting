60f19d95761e20f14279ffbe015381184f553e94
VAPopa7 <vlad.popa.2501@gmail.com>
2026-03-06 11:24:38 +0100

    data: removed NaN's in data for the sake of PCA

--- files/scripts/data_preprocessing.py
+++ files/scripts/data_preprocessing.py
@@ -12,6 +12,12 @@ df_recs['date'] = pd.to_datetime(df_recs['date'], format="%Y-%m-%d")
 df_recs = df_recs.set_index('date')
 df_yc = df_yc.set_index('date')
 
+# Identify columns with many missing values 
+nan_cols = df_yc.isna().sum()
+# print(nan_cols) # series is either inconsistent, or started later
+df_yc.drop(["1 mo", "2 mo", "4 mo", "20 yr", "30 yr"], axis=1, inplace=True)
+df_yc = df_yc.dropna() # drop the last dates containing NaN's
+
 # Note that the "exact" start day and end day of the recessions are lost
 df_recs = df_recs.reindex(df_yc.index, method='ffill')
 df_recs.to_csv(r"files\data\f_USREC.csv")
